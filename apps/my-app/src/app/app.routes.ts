fd5bbcfa456df04e22558af90f2bbf1850ec9de8
StanTeurlings <s_lings@hotmail.com>
2024-11-18 13:13:44 +0100

    start aboutpage

--- apps/my-app/src/app/app.routes.ts
+++ apps/my-app/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 import { UserListComponent } from './components/user/user-list/user-list.component';
 import { UserEditComponent } from './components/user/user-edit/user-edit.component';
 import { UserDetailComponent } from './components/user/user-detail/user-detail.component';
 
 export const appRoutes: Routes = [
     { path: '', pathMatch: "full", redirectTo: 'users' },
+    { path: 'about', pathMatch: "full", component: AboutComponent },
     { path: 'users', pathMatch: "full", component: UserListComponent },
     { path: 'users/new', pathMatch: "full", component: UserEditComponent },
     { path: 'users/:id', pathMatch: "full", component: UserDetailComponent },

8b8010db98a12d5b597048746a79e0d21b37b189
StanTeurlings <s_lings@hotmail.com>
2024-12-10 19:17:56 +0100

    start auth service\

--- libs/backend/auth/src/lib/auth/auth.service.ts
+++ libs/backend/auth/src/lib/auth/auth.service.ts
@@ -5,7 +5,7 @@ import {
 } from '@nestjs/common/exceptions';
 import { HttpStatus } from '@nestjs/common/enums';
 import {
-    user as userModel,
+    user as UserModel,
     userDocument
 } from '@avans-nx-workshop/backend/user';
 import { JwtService } from '@nestjs/jwt';
@@ -13,28 +13,19 @@ import { IuserCredentials, IuserIdentity } from '@avans-nx-workshop/shared/api';
 import { CreateuserDto } from '@avans-nx-workshop/backend/dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class AuthService {
     //
-    public currentUser$ = new BehaviorSubject<IuserIdentity | undefined>(undefined);
-
     private readonly logger = new Logger(AuthService.name);
-    private readonly CURRENT_USER = 'currentUser';
-
-    private readonly headers = new HttpHeaders({
-        'Content-Type': 'application/json'
-    });
 
     constructor(
-        @InjectModel(userModel.name) private userModel: Model<userDocument>,
+        @InjectModel(UserModel.name) private userModel: Model<userDocument>,
         private jwtService: JwtService
     ) {}
 
-    async validateuser(credentials: IuserCredentials): Promise<any> {
-        this.logger.log('validateuser');
+    async validateUser(credentials: IuserCredentials): Promise<any> {
+        this.logger.log('validateUser');
         const user = await this.userModel.findOne({
             emailAddress: credentials.emailAddress
         });
@@ -60,7 +51,6 @@ export class AuthService {
                     return {
                         _id: user._id,
                         name: user.name,
-                        lastName: user.lastName,
                         emailAddress: user.emailAddress,
                         profileImgUrl: user.profileImgUrl,
                         token: this.jwtService.sign(payload)
@@ -80,19 +70,10 @@ export class AuthService {
         this.logger.log(`Register user ${user.name}`);
         if (await this.userModel.findOne({ emailAddress: user.emailAddress })) {
             this.logger.debug('user exists');
-            throw new ConflictException('user already exist');
+            throw new ConflictException('User already exist');
         }
-        this.logger.debug('user not found, creating');
+        this.logger.debug('User not found, creating');
         const createdItem = await this.userModel.create(user);
         return createdItem;
-    }
-
-    getUserFromLocalStorage(): Observable<IuserIdentity>{
-        const localUser = JSON.parse(localStorage.getItem(this.CURRENT_USER) || '{}');
-        return of(localUser);
-    }
-
-    private saveUserToLocalStorage(user: IuserIdentity): void {  
-        localStorage.setItem(this.CURRENT_USER, JSON.stringify(user));
     }
 }
---
+++ libs/frontend/features/src/lib/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import { BehaviorSubject, catchError, map, Observable } from "rxjs";
+import { HttpHeaders } from "@angular/common/http";
+import { User } from "../user/user.model";
+import { IuserIdentity } from "@avans-nx-workshop/shared/api";
+import { environment } from '@avans-nx-workshop/shared/util-env';
+
+export class AuthService {
+    public currentUser$ = new BehaviorSubject<IuserIdentity | undefined> (undefined);
+    
+    private readonly CURRENT_USER = 'currentUser';
+
+    private readonly headers = new HttpHeaders({
+        'Content-Type': 'application/json'
+    });
+
+    login(email: string, password: string): Observable<IuserIdentity> {
+        console.log (`login at ${environment.dataApiUrl}login`);
+
+        return this.http
+        .post(
+            `${environment.dataApiUrl}login`,
+            { email, password },
+            { headers: this.headers }
+        )
+        .pipe(
+            map((response: any) => {
+                const user = { ...response } as User;
+                this.saveUserToLocalStorage(user);
+                this.currentUser$.next(user);
+                this.alertService.success('Logged in successfully');
+                return user;
+            }),
+            catchError((error: any) => {
+                console.log('error', error);
+                console.log('error.message:', error.message);
+                console.log('error.error.message:', error.error.message);
+                this.alertService.error(error.error.message || error.message);
+                return of(undefined); 
+            }   
+    }
+
+    getUserFromLocalStorage(): Observable<User> {
+        const localUser = JSON.parse(localStorage.getItem(this.CURRENT_USER));
+        return of(localUser);
+    }
+
+    private saveUserToLocalStorage(user: User): void {
+        localStorage.setItem(this.CURRENT_USER, JSON.stringify(user));
+    }
+}
